e3129291cd36a791b2c077ed3f18b7e200608519
RUDRAKSHA KUSHWAHA <kushwaharudraksha@gmail.com>
2023-04-04 18:20:28 +0530

    Update For.java
    Hello there ,I am just new to open source contributions , and trying my hands on it .  just give it a check please.
    Thank you

--- src/com/revature/fsd/examples/loops/For.java
+++ src/com/revature/fsd/examples/loops/For.java
@@ -3,7 +3,7 @@ package com.revature.fsd.examples.loops;
 public class For {
     public static void main(String args[]) {
         for (int i = 1; i < 6; i++) {
-            System.out.println(i);
+            System.out.print(i+"\n");
         }
     }
 }
